fc885ca3f842c173e46003701b6680cb16b271fc
Andrea Iob <andrea_iob@hotmail.com>
2021-06-17 09:24:59 +0200

    constants: remove unneeded includes

--- src/constants.hpp
+++ src/constants.hpp
@@ -25,12 +25,6 @@
 #ifndef __MINIMMERFLOW_CONSTANTS_HPP__
 #define __MINIMMERFLOW_CONSTANTS_HPP__
 
-#if ENABLE_MPI
-#include "communications.hpp"
-#endif
-
-#include <bitpit_containers.hpp>
-
 // Constants
 const int N_FIELDS = 5;
 
