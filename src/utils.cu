ed0c7ff7861eac7c758d118cf496aad54e4bed87
Andrea Iob <andrea_iob@hotmail.com>
2022-09-14 10:54:09 +0200

    utils: add function to evaluate the factorial

--- src/utils.cu
+++ src/utils.cu
@@ -23,3 +23,23 @@
 \*---------------------------------------------------------------------------*/
 
 #include "utils.hcu"
+
+namespace utils {
+
+/*!
+ * Compute the factorial of the specified number.
+ *
+ * \param[in] n is the argument for which the factorial has to be evaluated
+ * \result The factorial of the specified number.
+ */
+CUDA_HOST_DEVICE unsigned long factorial(unsigned long n)
+{
+    unsigned long factorial = 1;
+    for (unsigned long i = 1; i <= n; ++i) {
+        factorial *= i;
+    }
+
+    return factorial;
+}
+
+}
