ad8a2ff0258ba15a2dabcfe6a8f10b82aa250024
Andrea Iob <andrea_iob@hotmail.com>
2022-09-18 17:56:48 +0200

    containers: instantiate containers for long data type

--- src/containers.cpp
+++ src/containers.cpp
@@ -27,41 +27,49 @@
 #if ENABLE_CUDA == 0
 // Explicit instantiation
 template class ValueBaseStorage<bitpit::PiercedStorage<int, long>, int, int>;
+template class ValueBaseStorage<bitpit::PiercedStorage<long, long>, long, long>;
 template class ValueBaseStorage<bitpit::PiercedStorage<std::size_t, long>, std::size_t, std::size_t>;
 template class ValueBaseStorage<bitpit::PiercedStorage<double, long>, double, double>;
 template class ValueBaseStorage<bitpit::PiercedStorage<std::array<double, 3>, long>, std::array<double, 3>, double>;
 
 template class ValueBaseStorage<std::vector<int>, int, int>;
+template class ValueBaseStorage<std::vector<long>, long, long>;
 template class ValueBaseStorage<std::vector<std::size_t>, std::size_t, std::size_t>;
 template class ValueBaseStorage<std::vector<double>, double, double>;
 template class ValueBaseStorage<std::vector<std::array<double, 3>>, std::array<double, 3>, double>;
 
 template class ValuePiercedStorage<int>;
+template class ValuePiercedStorage<long>;
 template class ValuePiercedStorage<std::size_t>;
 template class ValuePiercedStorage<double>;
 template class ValuePiercedStorage<std::array<double, 3>, double>;
 
 template class ValueStorage<int>;
+template class ValueStorage<long>;
 template class ValueStorage<std::size_t>;
 template class ValueStorage<double>;
 template class ValueStorage<std::array<double, 3>, double>;
 
 template class BaseStorageCollection<ValuePiercedStorage<int, int>>;
+template class BaseStorageCollection<ValuePiercedStorage<long, long>>;
 template class BaseStorageCollection<ValuePiercedStorage<std::size_t, std::size_t>>;
 template class BaseStorageCollection<ValuePiercedStorage<double, double>>;
 template class BaseStorageCollection<ValuePiercedStorage<std::array<double, 3>, double>>;
 
 template class BaseStorageCollection<ValueStorage<int, int>>;
+template class BaseStorageCollection<ValueStorage<long, long>>;
 template class BaseStorageCollection<ValueStorage<std::size_t, std::size_t>>;
 template class BaseStorageCollection<ValueStorage<double, double>>;
 template class BaseStorageCollection<ValueStorage<std::array<double, 3>, double>>;
 
 template class PiercedStorageCollection<int, int>;
+template class PiercedStorageCollection<long, long>;
 template class PiercedStorageCollection<std::size_t, std::size_t>;
 template class PiercedStorageCollection<double, double>;
 template class PiercedStorageCollection<std::array<double, 3>, double>;
 
 template class StorageCollection<int>;
+template class StorageCollection<long>;
 template class StorageCollection<std::size_t>;
 template class StorageCollection<double>;
 template class StorageCollection<std::array<double, 3>, double>;
--- src/containers.cu
+++ src/containers.cu
@@ -32,58 +32,70 @@
 
 // Explicit instantiation
 template class ValueBaseStorage<bitpit::PiercedStorage<int, long>, int, int>;
+template class ValueBaseStorage<bitpit::PiercedStorage<long, long>, long, long>;
 template class ValueBaseStorage<bitpit::PiercedStorage<std::size_t, long>, std::size_t, std::size_t>;
 template class ValueBaseStorage<bitpit::PiercedStorage<double, long>, double, double>;
 template class ValueBaseStorage<bitpit::PiercedStorage<std::array<double, 3>, long>, std::array<double, 3>, double>;
 
 template class ValueBaseStorage<std::vector<int>, int, int>;
+template class ValueBaseStorage<std::vector<long>, long, long>;
 template class ValueBaseStorage<std::vector<std::size_t>, std::size_t, std::size_t>;
 template class ValueBaseStorage<std::vector<double>, double, double>;
 template class ValueBaseStorage<std::vector<std::array<double, 3>>, std::array<double, 3>, double>;
 
 template class ValuePiercedStorage<int>;
+template class ValuePiercedStorage<long>;
 template class ValuePiercedStorage<std::size_t>;
 template class ValuePiercedStorage<double>;
 template class ValuePiercedStorage<std::array<double, 3>, double>;
 
 template class ValueStorage<int>;
+template class ValueStorage<long>;
 template class ValueStorage<std::size_t>;
 template class ValueStorage<double>;
 template class ValueStorage<std::array<double, 3>, double>;
 
 template class BaseStorageCollection<ValuePiercedStorage<int, int>>;
+template class BaseStorageCollection<ValuePiercedStorage<long, long>>;
 template class BaseStorageCollection<ValuePiercedStorage<std::size_t, std::size_t>>;
 template class BaseStorageCollection<ValuePiercedStorage<double, double>>;
 template class BaseStorageCollection<ValuePiercedStorage<std::array<double, 3>, double>>;
 
 template class BaseStorageCollection<ValueStorage<int, int>>;
+template class BaseStorageCollection<ValueStorage<long, long>>;
 template class BaseStorageCollection<ValueStorage<std::size_t, std::size_t>>;
 template class BaseStorageCollection<ValueStorage<double, double>>;
 template class BaseStorageCollection<ValueStorage<std::array<double, 3>, double>>;
 
 template class PiercedStorageCollection<int, int>;
+template class PiercedStorageCollection<long, long>;
 template class PiercedStorageCollection<std::size_t, std::size_t>;
 template class PiercedStorageCollection<double, double>;
 template class PiercedStorageCollection<std::array<double, 3>, double>;
 
 template class StorageCollection<int>;
+template class StorageCollection<long>;
 template class StorageCollection<std::size_t>;
 template class StorageCollection<double>;
 template class StorageCollection<std::array<double, 3>, double>;
 
 template class DeviceCollectionDataCursor<int>;
+template class DeviceCollectionDataCursor<long>;
 template class DeviceCollectionDataCursor<std::size_t>;
 template class DeviceCollectionDataCursor<double>;
 
 template class DeviceCollectionDataConstCursor<int>;
+template class DeviceCollectionDataConstCursor<long>;
 template class DeviceCollectionDataConstCursor<std::size_t>;
 template class DeviceCollectionDataConstCursor<double>;
 
 template class DeviceStridedDataCursor<int>;
+template class DeviceStridedDataCursor<long>;
 template class DeviceStridedDataCursor<std::size_t>;
 template class DeviceStridedDataCursor<double>;
 
 template class DeviceStridedDataConstCursor<int>;
+template class DeviceStridedDataConstCursor<long>;
 template class DeviceStridedDataConstCursor<std::size_t>;
 template class DeviceStridedDataConstCursor<double>;
 
